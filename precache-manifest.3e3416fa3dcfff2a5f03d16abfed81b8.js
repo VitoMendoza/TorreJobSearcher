7d80a0185369f94a42540d47c66b7cca97419df7
Vito Mendoza <v_nm6@hotmail.com>
2020-05-09 02:12:00 -0400

    Updates

---
+++ precache-manifest.3e3416fa3dcfff2a5f03d16abfed81b8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c72f15135249d888c542e1306ec814e7",
+    "url": "/torrejobsearcher/index.html"
+  },
+  {
+    "revision": "ef0b32b635f10ebf8482",
+    "url": "/torrejobsearcher/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "3e83bf80af17ba96967b",
+    "url": "/torrejobsearcher/static/css/main.17643b97.chunk.css"
+  },
+  {
+    "revision": "ef0b32b635f10ebf8482",
+    "url": "/torrejobsearcher/static/js/2.600a21bf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/torrejobsearcher/static/js/2.600a21bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e83bf80af17ba96967b",
+    "url": "/torrejobsearcher/static/js/main.00aba621.chunk.js"
+  },
+  {
+    "revision": "54a2ddb088fda9c903f4",
+    "url": "/torrejobsearcher/static/js/runtime-main.5fcbcfd8.js"
+  }
+]);
